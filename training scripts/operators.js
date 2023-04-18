9618833695bde3cb5f6ea5bfb6170379855bc695
Ruslan-KozhakIn <ruslangeraklyon@gmail.com>
2023-04-19 02:48:12 +0300

    created operators script

---
+++ training scripts/operators.js
@@ -0,0 +1,31 @@
+// ariphmetic operators
+
+console.log(`a + b: ${5 + 3}`); // 8
+console.log(`a - b: ${5 - 3}`); // 2
+console.log(`a * b: ${5 * 3}`); // 15
+console.log(`a / b: ${20 / 4}`); // 5
+console.log(`a % b: ${5 % 3}`); // 2
+
+// ** exponentiation
+
+console.log(`a ** b: ${2 ** 8}`); // 256
+
+// logic operators
+
+console.log(`a > b: ${5 > 3}`); // true
+console.log(`a < b: ${5 < 3}`); // false
+console.log(`a >= b: ${5 >= 3}`); // true
+console.log(`a <= b: ${20 <= 20}`); // true
+
+
+
+console.log(`a == b: ${5 == 5}`); // true
+console.log(`a == b: ${5 == 3}`); // false
+console.log(`a != b: ${5 != 3}`); // true
+ 
+// stricr equals compare types also
+
+console.log(`a === b: ${'5' === 3}`); // false
+console.log(`a !== b: ${5 !== 3}`); // true
+
+
