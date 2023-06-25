a44e2c521308db6786d51152c771cb7c8321fafa
Ruslan-KozhakIn <ruslangeraklyon@gmail.com>
2023-06-26 02:37:43 +0300

    refresh function script

--- training scripts/functions.js
+++ training scripts/functions.js
@@ -2,7 +2,7 @@
 
 // ............Function declaration: function name(parameters) {....body....}
 
-function greeting() {
+/*function greeting() {
     console.log('Hello everyone!');
 }
 
@@ -77,7 +77,141 @@ changeArray(arr1, function(n){return n + 1}); //  using expression
 changeArray(arr2, (n) => n -1);               //  using arrow
 
 console.log('arr 1 changed:', arr1);
-console.log('arr 2 changed:', arr2);
+console.log('arr 2 changed:', arr2); */
+
+  
+// REMEMBERING FUNCTIONS
+
+// Функція для того , щоб можна було викликати
+// певний блок кода по імені не копіюючи його!
+
+function showMessage() {
+    console.log("I like JavaScript!");
+}
+
+showMessage(); // викликання ф-ції по імені, щоб вона спрацювала.
 
 
-function (a);   
+// function to show a random value 0-10
+
+
+
+function showRandomNumberTo10() {   //назва ф-ції відповідає дії, ку вона виконує set - встановлюємо,check, show, get - повертаємо(return);
+    console.log(Math.floor(Math.random() * 11))
+
+}               
+
+showRandomNumberTo10();
+
+// function parameters: name(param1, param2) {.......}
+
+function showRandomNumberToMax(max) {   //назва ф-ції відповідає дії, яку вона виконує set - встановлюємо,check, show, get - повертаємо(return);
+    console.log(Math.floor(Math.random() * max + 1))
+
+}               
+
+showRandomNumberToMax(100);
+showRandomNumberToMax(30);
+
+// show count of value in the array
+
+
+const arr = [3, 6, 1, 3, 7, 3, 8, 2, 3, 8];
+const prices = [140, 10, 44, 10, 120, 5, 44, 44];
+
+function showCount(array, value) {
+    
+    alert("Value count: " + array.filter((i) => i === value).length);
+
+}
+
+console.log("Random value: " + getRandomNumberInRange(0, 10)); // hoisting - спливання функції до її оголошення .
+
+showCount(arr, 3);
+showCount(prices, 44);
+
+
+// return operator: повертає значення з ф-ції і виходить з неї---------.
+
+function getRandomNumberInRange(min, max) {   //назва ф-ції відповідає дії, яку вона виконує set - встановлюємо,check, show, get - повертаємо(return);
+   // console.log("Before return!");
+    return Math.floor(min + Math.random() * (max - min + 1));
+    // console.log("After return!"); // не спрацює, бо фу-ція завершилась. 
+
+}    
+
+//  showRandomNumberInRange(10, 20);
+// showRandomNumberInRange(80, 100);
+ 
+// initialize array with random value (10...40)
+
+for (i = 0; i < prices.length; i++) {
+
+    prices[i] = getRandomNumberInRange(10, 40);
+}
+
+
+console.log("Prices: ", prices);
+
+alert(getRandomNumberInRange(10, 40));
+
+console.log(getRandomNumberInRange(10, 40));
+
+
+
+function getRandomNumberInRange(min = 0, max = 10) {  // дефолтні параметри 
+    return Math.floor(min + Math.random() * (max - min + 1));
+}
+
+console.log("Random value: " + getRandomNumberInRange());
+//  same result
+console.log("Random value: " + getRandomNumberInRange(0, 10));
+
+
+// hoisting змінних
+console.log(salary);
+var salary = 1200;
+console.log(salary);
+
+//  ******  callback фун-ції - передавання фу-ції у функцію .
+
+
+
+// function getModule(x) { return Math.abs(x); }; // модуль числа ( від'ємне значення переведе у позитивне)
+function getSqrt(x) { return Math.sqrt(x); }; // корінь квадратний
+// function getNegative(x) { return -x; };
+
+
+function changeArray(array, action) {
+    for (let i = 0; i < array.length; i++) {
+    array[i] = action(array[i]);
+    
+    }
+}
+
+let members = [4, 5, 7, -5, 80, 22];
+changeArray(members, getIncrement);
+// changeArray(members, getModule);
+// changeArray(members, getNegative);
+
+
+
+
+// 1 - Function declaration  
+function getIncrement(x) { return x + 1; };
+
+
+// 2 - Function expression 
+const getNegative = function (x) { return -x; };
+
+// 3 - anonymous functions
+
+changeArray(members, function (x) { return Math.abs(x); });// модуль числа ( від'ємне значення переведе у позитивне) 
+changeArray(members, function (x) { return x * x; });
+
+
+// 4 - arrow functions 
+
+changeArray(members, (x) => x * 2);
+
+console.log("Change array: ", members);
